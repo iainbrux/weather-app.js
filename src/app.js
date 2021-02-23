4a486c7999da9675799bbb1181f3b0a5fb127f83
Iain Brookes <me@iainbrux.com>
2021-02-23 17:09:35 +0000

    added lat and lon from promise

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const displayWeather = document.querySelector('.display');
 
 async function getWeather(input) {
-    let city = input;
-    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=62d9f7279fdb0e1b7b37a6dfead3cd52`, {mode: 'cors'});
-	 let weather = await response.json();
-	 console.log(weather)
+	let city = input;
+	let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=62d9f7279fdb0e1b7b37a6dfead3cd52`, {mode: 'cors'});
+	let retrieved = await response.json();
+	let lat = retrieved.coord.lat;
+	let lon = retrieved.coord.lon;
+	
+	let weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=62d9f7279fdb0e1b7b37a6dfead3cd52`, {mode: 'cors'});
+    let weatherRetrieved = await weatherResponse.json();
+	console.log(weatherRetrieved) //could be written into its own function from weatherResponse
+	 
 }
 
 getWeather('chorley')
